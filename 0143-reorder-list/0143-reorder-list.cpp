1b5a4e32bd537b71a04bdceb91c6033d7011a54f
stevengnb <131536921+stevengnb@users.noreply.github.com>
2024-08-21 15:29:34 +0700

    Time: 33 ms (17.41%), Space: 23.2 MB (29.19%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -30,32 +30,16 @@ public:
             stack.push(slow);
         }
 
-        // cout << "temp = " << temp->val << endl;
         temp->next = NULL;
 
         while(head && !stack.empty()) {
             ListNode *realNext = head->next;
-
             head->next = stack.top();
             stack.pop();
             head->next->next = realNext;
-            // cout << "head - val = " << head->val << endl;
-            // cout << "head - next = " << head->next->val << endl; 
             last = head;
             head = realNext;
         }
-
-        // if(last) cout << last->val << " - kajsdsda" << endl;
-        // if(last->next) cout << last->next->val << " - kajsdsda" << endl;
-        // if(head) cout << "haed = " << head->val << endl;
-        // last = last->next;
-
-        // while (!stack.empty()) {
-        //     cout << stack.top()->val <<" <- ini";
-        //     last->next = stack.top();
-        //     stack.pop();
-        //     last = last->next;
-        // }
 
         if(last->next && !stack.empty()) {
             last->next->next = stack.top();
