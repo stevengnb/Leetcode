2c862e7f1e91d5e41a2d58b782862ac3dcfff7b8
stevengnb <131536921+stevengnb@users.noreply.github.com>
2024-08-25 14:01:21 +0700

    Time: 360 ms (55.07%), Space: 177.4 MB (55.06%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,95 @@
+class LRUCache {
+struct Node {
+    int key;
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val, int key) : val(val), key(key), next(nullptr), prev(nullptr) {}
+};
+
+public:
+    unordered_map<int, Node*> map;
+    Node *head = nullptr;
+    Node *tail = nullptr;
+    int max;
+
+    LRUCache(int capacity) {
+        max = capacity;
+    }
+    
+    int get(int key) {
+        if(map.find(key) == map.end()) return -1;
+        
+        Node *updateNode = map[key];
+        update(updateNode, updateNode->val);
+
+        return updateNode->val;
+    }
+
+    void update(Node *updateNode, int value) {
+        Node *newNode = new Node(value, updateNode->key);
+
+        if(head == tail) {
+            head = tail = newNode;
+        } else if(head == updateNode) {
+            head = head->next;
+        } else if(tail == updateNode) {
+            tail = tail->prev;
+        } 
+        
+        if(updateNode->prev) updateNode->prev->next = updateNode->next;
+        if(updateNode->next) updateNode->next->prev = updateNode->prev;
+        updateNode = nullptr;
+
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+
+        map[newNode->key] = newNode;
+    }
+    
+    void put(int key, int value) {
+        if(map.find(key) != map.end()) {
+            update(map[key], value);
+        } else {
+            Node *newNode = new Node(value, key);
+
+            if(map.size() >= max) {
+                map.erase(head->key);
+                head = head->next;
+                // head->prev = nullptr;
+            } 
+
+            if(!head) {
+                head = tail = newNode;
+            } else if(head == tail) {
+                head->next = newNode;
+                newNode->prev = head;
+                tail = newNode;
+            } else {
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+            map[key] = newNode;
+        }
+
+        // Node *curr = head;
+        // while(curr) {
+        //     cout << "val = " << curr->val << endl;
+        //     curr = curr->next;
+        // }
+
+        // for (auto &mapp : map) {
+        //     cout << "key = " << mapp.first << ", value = " << mapp.second->val << endl;
+        // }
+        // cout << endl << endl;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
