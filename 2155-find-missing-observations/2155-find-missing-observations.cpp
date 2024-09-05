d2b1cca7518768211296ca75dd5b61df09fd28dd
stevengnb <131536921+stevengnb@users.noreply.github.com>
2024-09-05 10:59:45 +0700

    Time: 103 ms (37.5%), Space: 126.4 MB (8.58%) - LeetHub

---
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int totalMissing, total = 0, quantity = n + rolls.size();
+        vector<int> vect;
+
+        for(int roll : rolls) total += roll;
+        totalMissing = (quantity * mean) - total;
+
+        int average = totalMissing/(n-vect.size());
+        if(n * 6 < totalMissing || average == 0) return vect;
+
+        while(totalMissing > 0) {
+            average = totalMissing/(n-vect.size());
+            if(totalMissing >= average && vect.size() < n - 1) {
+                vect.push_back(average);
+                totalMissing -= average;
+            } else {
+                vect.push_back(totalMissing);
+                totalMissing -= totalMissing;
+            }
+        }
+
+        return vect;
+    }
+};
