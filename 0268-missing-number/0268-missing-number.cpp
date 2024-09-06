47333314aab02dfffd51bab806347700534d6219
stevengnb <131536921+stevengnb@users.noreply.github.com>
2024-09-06 10:37:16 +0700

    Time: 28 ms (7.77%), Space: 28.1 MB (5.11%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_set<int> set{nums.begin(), nums.end()};
+
+        for(int i = 0; i <= nums.size(); i++) {
+            if(set.find(i) == set.end()) return i;
+        }
+
+        return -1;
+    }
+};
