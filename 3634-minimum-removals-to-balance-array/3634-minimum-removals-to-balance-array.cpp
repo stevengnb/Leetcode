9f2bb7b9db1ecdd361417c74ae4717ceefdc74ca
Steven <131536921+stevengnb@users.noreply.github.com>
2026-02-26 11:12:52 +0700

    Time: 388 ms (5.7%), Space: 105.1 MB (51.39%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -4,22 +4,12 @@ public:
         int len = nums.size();
         if (len <= 1) return 0;
         sort(nums.begin(), nums.end());
-        // FORMULA = max <= min * k
-
-        // expand ketika max <= min * k
-        // shrink ketika max > min * kx
-
-        // 4 6
         int right = 0;
         int maxVal = 0;
 
         for (int left = 0; left < len; left++) {
-            // cout << "left = " << left << ", val = " << nums[left] << endl;
-            // cout << "right = " << right << ", val = " << nums[right] << endl;
             while (nums[right] <= (long long)nums[left]*k) {
-                // cout << "right didalem= " << right << ", val = " << nums[right] << endl;
                 if (nums[right] <= (long long)nums[left]*k) {
-                    // cout << "gamasuk sini?" << endl;
                     maxVal = max(maxVal, right-left+1);
                 }
                 right++;
@@ -30,10 +20,6 @@ public:
             cout << endl;
         }
 
-        // cout << "max window = " << maxVal << endl;
-
         return len - maxVal;
-
-        // 18 <= k * 16
     }
 };
