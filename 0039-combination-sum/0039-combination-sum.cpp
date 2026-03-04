ccb38abff6d0904c0db30759591f9cd7a442d98b
Steven <131536921+stevengnb@users.noreply.github.com>
2026-03-04 12:48:38 +0700

    Time: 4 ms (38.22%), Space: 14.1 MB (74.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -4,26 +4,24 @@ public:
         vector<vector<int>> ans;
         vector<int> path;
 
-        backtrack(ans, candidates, path, 0, target);
+        backtrack(ans, candidates, path, 0, target, 0);
 
         return ans;
     }
 
-    void backtrack(vector<vector<int>>& ans, vector<int>& candidates, vector<int>& path, int sum, int target) {
+    void backtrack(vector<vector<int>>& ans, vector<int>& candidates, vector<int>& path, int sum, int target, int start) {
         if (sum > target) return;
         else if (sum == target) {
             ans.push_back(path);
             return;
         }
 
-        for (int i = 0; i < candidates.size(); i++) {
+        for (int i = start; i < candidates.size(); i++) {
             int cand = candidates[i];
-            if (!path.empty() && path.back() < cand) continue;
-
             sum += cand;
             path.push_back(cand);
 
-            backtrack(ans, candidates, path, sum, target);
+            backtrack(ans, candidates, path, sum, target, i);
 
             path.pop_back();
             sum -= cand;
