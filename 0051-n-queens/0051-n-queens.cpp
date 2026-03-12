78e0ff3952961d1c91e0a097259cbfcb629f8ef9
Steven <131536921+stevengnb@users.noreply.github.com>
2026-03-12 15:04:36 +0700

    Time: 6 ms (19.72%), Space: 10.1 MB (75.95%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> path(n, string(n, '.'));
+
+        backtrack(ans, path, 0, 0, n, 0);
+
+        return ans;
+    }
+
+    void backtrack(vector<vector<string>> &ans, vector<string> &path, int x, int y, int n, int queen) {
+        if (queen == n) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (isValid(x, j, path, n)) {
+                path[x][j] = 'Q';
+                backtrack(ans, path, x+1, y, n, queen+1);
+                path[x][j] = '.';
+            }
+        }
+    }
+
+    bool isValid(int x, int y, vector<string> &path, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (path[i][j] == 'Q') {
+                    if (i == x || j == y) return false;
+                    else if (abs(i-x) == abs(j-y)) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
