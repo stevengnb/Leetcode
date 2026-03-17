62887180f716095a36cd9ace62db7e976c95baa9
Steven <131536921+stevengnb@users.noreply.github.com>
2026-03-17 16:06:54 +0700

    Time: 15 ms (6.51%), Space: 11.5 MB (18.17%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        string path;
+
+        backtrack(ans, path, s, 0, 0, "", 0);
+
+        return ans;
+    }
+
+    void backtrack(vector<string> &ans, string &path, string &s, int start, int dots, string curr, int sum) {
+        if (start > s.size()) return;
+        if (curr != "" && (stoi(curr) < 0 || stoi(curr) > 255)) return;
+        if (curr.size() >= 2 && curr[0] == '0') return;
+        // cout << "=== BACKTRACK ===" << endl;
+        // cout << "path = " << path << endl;
+        // cout << "s = " << s << endl;
+        // cout << "start = " << start << endl;
+        // cout << "dots = " << dots << endl << endl;
+
+        if (dots == 4 && sum == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            int len = path.size();
+            string now = s.substr(start, i);
+
+            path += now;
+            if (dots != 3) path += ".";
+            backtrack(ans, path, s, start+i, dots+1, now, sum+i);
+            path.resize(len);
+        }
+    }
+};
