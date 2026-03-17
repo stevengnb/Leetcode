dcd37bb67d35fb3e0bd2ed284b65626f5320aa6c
Steven <131536921+stevengnb@users.noreply.github.com>
2026-03-17 16:13:11 +0700

    Time: 8 ms (8.82%), Space: 11.4 MB (19.24%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -10,7 +10,7 @@ public:
     }
 
     void backtrack(vector<string> &ans, string &path, string &s, int start, int dots, string curr, int sum) {
-        if (start > s.size()) return;
+        // if (start > s.size()) return;
         if (curr != "" && (stoi(curr) < 0 || stoi(curr) > 255)) return;
         if (curr.size() >= 2 && curr[0] == '0') return;
 
@@ -20,6 +20,8 @@ public:
         }
 
         for (int i = 1; i <= 3; i++) {
+            if (start + i > s.size()) break;
+
             int len = path.size();
             string now = s.substr(start, i);
 
