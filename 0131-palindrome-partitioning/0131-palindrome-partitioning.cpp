a5b0d4c9b1d9cf3ab87bcf736a821125f3afaaa9
Steven <131536921+stevengnb@users.noreply.github.com>
2026-03-18 22:29:24 +0700

    Time: 46 ms (55.07%), Space: 57.4 MB (57.69%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> path;
+
+        backtrack(ans, path, 0, s);
+
+        return ans;
+    }
+
+    void backtrack(vector<vector<string>> &ans, vector<string> &path, int start, string s) {
+        if (start == s.size()) {
+            ans.push_back(path);
+        }
+
+        for (int i = 1; i <= s.size(); i++) {
+            // printPath(path);
+            // cout << "i now = " << i << endl;
+            // cout << "start now = " << start << endl << endl;
+            if (start + i > s.size()) break;
+            string add = s.substr(start, i);
+            if (!isPalindrome(add)) continue;
+
+            path.push_back(add);
+            backtrack(ans, path, start+i, s);
+            path.pop_back();
+        }
+
+    }
+
+    // void printPath(vector<string> path) {
+    //     cout << "PATH = ";
+    //     for (string s : path) {
+    //         cout << " " << s;
+    //     }
+    //     cout << endl;
+    // }
+
+    bool isPalindrome(string s) {
+        int len = s.size();
+        for (int i = 0; i < len/2; i++) {
+            if (s[i] != s[len-i-1]) return false;
+        }
+        return true;
+    }
+};
