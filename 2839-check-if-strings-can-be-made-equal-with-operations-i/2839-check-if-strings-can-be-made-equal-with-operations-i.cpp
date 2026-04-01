b394038869f8aec0014f643bd907c374b7f1a776
Steven <131536921+stevengnb@users.noreply.github.com>
2026-04-01 20:12:38 +0700

    Time: 0 ms (100%), Space: 8.2 MB (90.45%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -7,7 +7,6 @@ public:
             if (s1[i] == s2[i]) continue;
             
             if (s1[i+2] == s2[i]) {
-                // swap?
                 int temp = s1[i+2];
                 s1[i+2] = s1[i];
                 s1[i] = temp;
@@ -17,22 +16,6 @@ public:
         }
 
         for (int i = len-2; i < len; i++) if (s1[i] != s2[i]) return false;
-
-        /*
-            ifjz -> jzfi
-
-            i = 0
-            swap
-            s1 = JFIZ
-            s2 = JZFI
-
-            i = 1
-            swap
-            s1 = JZIF
-            s2 = JZFI
-
-        */
-
         return true;
     }
 };
